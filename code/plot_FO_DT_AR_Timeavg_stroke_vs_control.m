91262721eb63fb0a25281a09eb25cba6c7985049
Emily Olafson <emily.olafson@mail.mcgill.ca>
2021-06-16 15:06:38 -0400

    violinplots with asterisks

--- code/plot_FO_DT_AR_Timeavg_stroke_vs_control.m
+++ code/plot_FO_DT_AR_Timeavg_stroke_vs_control.m
@@ -28,11 +28,10 @@ violin(5).ViolinColor=[0.4 0.4 0.8]
 violin(6).ViolinColor=[1 0.4 0.4]
 violin(7).ViolinColor=[0.4 0.4 0.8]
 violin(8).ViolinColor=[1 0.4 0.4]
+xticks(1:8)
 
-xticks(1:2:12)
-xticklabels({'State 1','State 2','State 3', 'State 4'})
-set(gca, 'FontSize', 15)
-
+yt = get(gca, 'YTick');
+xt = get(gca, 'XTick');
 
 [h, p(1), ci, stats]=ttest2(mean(stroke_FO1, 2, 'omitnan'),mean(control_FO1, 2))
 [h, p(2), ci, stats]=ttest2(mean(stroke_FO2,  2, 'omitnan'),mean(control_FO2, 2))
@@ -40,6 +39,31 @@ set(gca, 'FontSize', 15)
 [h, p(4), ci, stats]=ttest2(mean(stroke_FO4,  2, 'omitnan'),mean(control_FO4, 2))
 
 [h, ~, ~, p_adj] = fdr_bh(p, 0.05,'pdep')
+old=0
+for i=1:4
+    xtickindex=i+old
+    old=i;
+    signif=p(i)<0.05
+    if signif
+        plot(xt([xtickindex xtickindex+1]), [1 1]*max(yt)*1.1, '-k',  mean(xt([xtickindex xtickindex+1])), max(yt)*1.15, '*k')
+    end
+end
+
+old=0
+
+for i=1:4
+    xtickindex=i+old
+    old=i;
+    signif=p_adj(i)<0.05;
+    if signif
+        plot(xt([xtickindex xtickindex+1]), [1 1]*max(yt)*1.1, '-k',  mean(xt([xtickindex xtickindex+1])), max(yt)*1.15, '*k');
+        plot(xt([xtickindex xtickindex+1]), [1 1]*max(yt)*1.1, '-k',  mean(xt([xtickindex xtickindex+1]))+0.1, max(yt)*1.15, '*k');
+    end
+end
+xticklabels({'State 1','    ','State 2','     ','State 3', '','State 4'})
+set(gca, 'FontSize', 15)
+
+
 
 save(strcat(resdir, 'p_adjusted_Timeavg_FO_stroke_vs_control.mat', 'p_adj'))
 save(strcat(resdir, 'p_unc_Timeavg_FO_stroke_vs_control.mat', 'p'))
@@ -69,11 +93,13 @@ violin(5).ViolinColor=[0.4 0.4 0.8]
 violin(6).ViolinColor=[1 0.4 0.4]
 violin(7).ViolinColor=[0.4 0.4 0.8]
 violin(8).ViolinColor=[1 0.4 0.4]
-xticks(1:2:12)
-xticklabels({'State 1','State 2','State 3', 'State 4'})
+xticklabels({'State 1','    ','State 2','     ','State 3', '','State 4'})
 set(gca, 'FontSize', 15)
-ylabel('Dwell time (TRs)')
+xticks(1:8)
 
+ylabel('Dwell time (TRs)')
+yt = get(gca, 'YTick');
+xt = get(gca, 'XTick');
 [h, p(3), ci, stats]=ttest2(mean(dwell_avg_stroke(:,:,3),2, 'omitnan'),mean(dwell_avg_control(:,:,3),2))
 [h, p(4), ci, stats]=ttest2(mean(dwell_avg_stroke(:,:,4),2, 'omitnan'),mean(dwell_avg_control(:,:,4),2))
 [h, p(1), ci, stats]=ttest2(mean(dwell_avg_stroke(:,:,1),2, 'omitnan'),mean(dwell_avg_control(:,:,1), 2))
@@ -81,10 +107,33 @@ ylabel('Dwell time (TRs)')
 
 [h, ~, ~, p_adj] = fdr_bh(p, 0.05,'pdep')
 
+old=0
+for i=1:4
+    xtickindex=i+old
+    old=i;
+    signif=p(i)<0.05
+    if signif
+        plot(xt([xtickindex xtickindex+1]), [1 1]*max(yt)*1.1, '-k',  mean(xt([xtickindex xtickindex+1])), max(yt)*1.15, '*k')
+    end
+end
+
+old=0
+
+for i=1:4
+    xtickindex=i+old
+    old=i;
+    signif=p_adj(i)<0.05;
+    if signif
+        plot(xt([xtickindex xtickindex+1]), [1 1]*max(yt)*1.1, '-k',  mean(xt([xtickindex xtickindex+1])), max(yt)*1.15, '*k');
+        plot(xt([xtickindex xtickindex+1]), [1 1]*max(yt)*1.1, '-k',  mean(xt([xtickindex xtickindex+1]))+0.1, max(yt)*1.15, '*k');
+    end
+end
+
+
 
 save(strcat(resdir, 'p_adjusted_Timeavg_DT_stroke_vs_control.mat'), 'p_adj')
 save(strcat(resdir, 'p_unc_Timeavg_DR_stroke_vs_control.mat'), 'p')
-saveas(gcf,strcat(figdir, 'DT_Timeavg_stroke_vs_control.png')
+saveas(gcf,strcat(figdir, 'DT_Timeavg_stroke_vs_control.png'))
 
 %% Appearance rate
 close all;
@@ -111,16 +160,40 @@ violin(6).ViolinColor=[1 0.4 0.4]
 violin(7).ViolinColor=[0.4 0.4 0.8]
 violin(8).ViolinColor=[1 0.4 0.4]
 
-xticks(1:2:12)
-xticklabels({'State 1','State 2','State 3', 'State 4'})
+xticks(1:8)
+xticklabels({'State 1','    ','State 2','     ','State 3', '','State 4'})
 set(gca, 'FontSize', 15)
 clear p
 [h, p(1), ci, stats]=ttest2(mean(stroke_appearance1,2, 'omitnan'),mean(control_appearance1,2))
 [h, p(2), ci, stats]=ttest2(mean(stroke_appearance2,2, 'omitnan'),mean(control_appearance2,2))
 [h, p(3), ci, stats]=ttest2(mean(stroke_appearance3,2, 'omitnan'),mean(control_appearance3,2))
 [h, p(4), ci, stats]=ttest2(mean(stroke_appearance4, 2,'omitnan'),mean(control_appearance4,2))
+yt = get(gca, 'YTick');
+xt = get(gca, 'XTick');
 
 [h, ~, ~, p_adj] = fdr_bh(p, 0.05,'pdep')
+old=0
+for i=1:4
+    xtickindex=i+old
+    old=i;
+    signif=p(i)<0.05
+    if signif
+        plot(xt([xtickindex xtickindex+1]), [1 1]*max(yt)*1.1, '-k',  mean(xt([xtickindex xtickindex+1])), max(yt)*1.15, '*k')
+    end
+end
+
+old=0
+
+for i=1:4
+    xtickindex=i+old
+    old=i;
+    signif=p_adj(i)<0.05;
+    if signif
+        plot(xt([xtickindex xtickindex+1]), [1 1]*max(yt)*1.1, '-k',  mean(xt([xtickindex xtickindex+1])), max(yt)*1.15, '*k');
+        plot(xt([xtickindex xtickindex+1]), [1 1]*max(yt)*1.1, '-k',  mean(xt([xtickindex xtickindex+1]))+0.1, max(yt)*1.15, '*k');
+    end
+end
+
 
 save(strcat(resdir, 'p_adjusted_Timeavg_AR_stroke_vs_control.mat'), 'p_adj')
 save(strcat(resdir, 'p_unc_Timeavg_R_stroke_vs_control.mat'), 'p')
